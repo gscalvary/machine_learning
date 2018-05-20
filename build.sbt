a9ab2bfd302301a3689f92fa1883177ba9ec1249
Christopher Oliver <coliver650@gmail.com>
2018-05-20 15:18:02 -0500

    Simple least square linear regression implemented!

--- build.sbt
+++ build.sbt
@@ -6,8 +6,7 @@ scalaVersion := "2.12.5"
 
 libraryDependencies ++= Seq(
 	"org.scalatest" %% "scalatest" % "3.0.5" % "test",
-	"org.scalanlp" %% "breeze" % "0.13.2",
-	"com.typesafe.scala-logging" %% "scala-logging" % "3.9.0"
+	"org.scalanlp" %% "breeze" % "0.13.2"
 )
 
 resolvers += "Sonatype Releases" at "https://oss.sonatype.org/content/repositories/releases/"
--- src/main/scala/supervisedLearning/LeastSquaresLinearRegression.scala
+++ src/main/scala/supervisedLearning/LeastSquaresLinearRegression.scala
@@ -1,25 +1,45 @@
 package supervisedLearning
 
-import breeze.linalg.{DenseMatrix, DenseVector, inv}
+import breeze.linalg.{DenseMatrix, DenseVector, Transpose, inv}
 
-class LeastSquaresLinearRegression(val observations: DenseMatrix[Double], val labels: DenseVector[Double], val offset: Double = 0) {
+/**
+  * Given a set of observations and a label associated with each, compute a hyperplane through the observation
+  * dimensions such that the sum of the squares of the difference between each observation and its closest point on the
+  * hyperplane is minimized.  This works best if there are many more observations than dimensions to the problem.
+  *
+  * @param observations - observed data points from some problem domain
+  * @param labels - labels associated with each observation
+  * @param offset - a value that may be used to shift the computed hyperplane
+  */
+class LeastSquaresLinearRegression(
+		val observations: DenseMatrix[Double],
+		val labels: DenseVector[Double],
+		val offset: Double = LeastSquaresLinearRegression.DEFAULT_OFFSET)
+{
 	
 	// Public Methods
+	/**
+	  * Given an observation gathered in the same way as the observations generated in the training set, compute a
+	  * label.
+	  *
+	  * @param observation - some observed datapoint
+	  * @return a label for the observation
+	  */
 	def getLabel(observation: DenseVector[Double]): Option[Double] = {
 		
-		if (freeParameters.isEmpty) {
+		if (weights.isEmpty) {
 			return None
 		}
 		
-		if (observation.length != freeParameters.get.length - 1) {
+		if (observation.length != weights.get.length - 1) {
 			return None
 		}
 		
-		Some(computeObservationWithOffset(observation, 1) dot freeParameters.get)
+		Some(computeObservationWithOffset(observation.t, LeastSquaresLinearRegression.DEFAULT_OFFSET) dot weights.get)
 	}
 	
 	// Private Methods
-	private def computeFreeParameters(): Option[DenseVector[Double]] = {
+	private def computeWeights(): Option[DenseVector[Double]] = {
 		
 		if (observationsWithOffsets.isEmpty) {
 			return None
@@ -29,42 +49,45 @@ class LeastSquaresLinearRegression(val observations: DenseMatrix[Double], val la
 		
 		try {
 			val wls = inv(observationsWithOffsetsTraspose * observationsWithOffsets.get) * observationsWithOffsetsTraspose * labels
-			return Some(wls)
+			Some(wls)
 		} catch {
-			case singular: breeze.linalg.MatrixSingularException => singular.printStackTrace()
+			case singular: breeze.linalg.MatrixSingularException => None
 		}
-		
-		None
 	}
 	
 	private def computeObservationsWithOffsets(): Option[DenseMatrix[Double]] = {
 		
-		if (observations.cols < observations.rows + 1) {
+		if (observations.rows < observations.cols + 1) {
 			return None
 		}
 		
-		val owo = DenseMatrix.zeros[Double](observations.rows + 1, observations.cols)
+		val owo = DenseMatrix.zeros[Double](observations.rows, observations.cols + 1)
 		
-		for (i <- 0 until observations.cols) {
-			owo(::, i) := computeObservationWithOffset(observations(::,i), offset)
+		for (i <- 0 until observations.rows) {
+			owo(i, ::) := computeObservationWithOffset(observations(i,::), offset).t
 		}
 		
 		Some(owo)
 	}
 	
-	private def computeObservationWithOffset(observation: DenseVector[Double], offset: Double): DenseVector[Double] = {
+	private def computeObservationWithOffset(observation: Transpose[DenseVector[Double]], offset: Double): DenseVector[Double] = {
 		
-		val columnArray = new Array[Double](observation.length + 1)
-		columnArray(0) = offset
+		val row = new Array[Double](observation.t.length + 1)
+		row(0) = offset
 		
-		for (i <- 0 until observation.length) {
-			columnArray(i + 1) = observation(i)
+		for (i <- 0 until observation.t.length) {
+			row(i + 1) = observation(i)
 		}
 		
-		DenseVector(columnArray)
+		DenseVector(row)
 	}
 	
 	// Private Fields
 	private val observationsWithOffsets: Option[DenseMatrix[Double]] = computeObservationsWithOffsets()
-	private val freeParameters: Option[DenseVector[Double]] = computeFreeParameters()
+	private val weights: Option[DenseVector[Double]] = computeWeights()
+}
+
+object LeastSquaresLinearRegression
+{
+	val DEFAULT_OFFSET: Double = 1
 }
