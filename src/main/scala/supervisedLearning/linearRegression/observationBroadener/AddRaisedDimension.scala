41313961bcdc4fdfdc2acd32a5999f0c88ec6aaf
Christopher Oliver <coliver650@gmail.com>
2018-06-02 12:54:11 -0500

    Add comments to the new observation broadener.

--- src/main/scala/supervisedLearning/linearRegression/observationBroadener/AddRaisedDimension.scala
+++ src/main/scala/supervisedLearning/linearRegression/observationBroadener/AddRaisedDimension.scala
@@ -2,6 +2,15 @@ package supervisedLearning.linearRegression.observationBroadener
 import breeze.linalg.DenseVector
 
 object AddRaisedDimension extends CreatesObservationDimension {
+	
+	/**
+	  *
+	  * @param index a 0-based index used to determine which dimension of the observation should be used as a basis for
+	  *              the new dimension
+	  * @param observation an observation to which we would like to add a dimension
+	  * @param power the power to which we would like to raise a given dimension to create a new dimension
+	  * @return an observation
+	  */
 	override def getExtendedObservation(index: Int, observation: DenseVector[Double], power:Double*): DenseVector[Double] = {
 		
 		if (super.isIndexOutOfBounds(index, observation)) {
